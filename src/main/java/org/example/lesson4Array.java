1d57253f7c48ba2e9571059973ac4680ca4edca1
Jessi <jessi.nowski@hotmail.de>
2024-01-24 12:58:14 +0100

    updated files day 3.1

--- src/main/java/org/example/lesson4Array.java
+++ src/main/java/org/example/lesson4Array.java
@@ -1,12 +1,10 @@
 package org.example;
 
-import java.util.Arrays;
-
 public class lesson4Array {
     public static void main(String[] args) {
 
         // Step 1: Create an array with numbers from 1 to 10 and print it to the console
-        int[] numbers = {1,2,3,4,5,6,7,8,9,10};
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
         printArray(numbers);
 
@@ -21,7 +19,7 @@ public class lesson4Array {
         findMax(numbers);
 
         //Step 4: Create a second array with numbers from 11 to 20.
-        int[] numbers2={11,12,13,14,15,16,17,18,19,20};
+        int[] numbers2 = {11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
 
         //Step 5: Perform element-wise addition of both arrays and store the result in a third array. Print the result array to the console.
         System.out.println();
@@ -32,27 +30,27 @@ public class lesson4Array {
 
     // Step 1: Create an array with numbers from 1 to 10 and print it to the console
 
-    public static void printArray(int[] arr){
-        for(int number:arr){
+    public static void printArray(int[] arr) {
+        for (int number : arr) {
             System.out.println(number);
         }
     }
 
     //Step 2: Calculate the sum of all numbers in the array and print the result to the console
-     public static void sumUpArray(int[] arr){
-         int sum =0;
-         for(int number:arr){
-             sum += number;
-         }
-         System.out.println(sum);
-     }
+    public static void sumUpArray(int[] arr) {
+        int sum = 0;
+        for (int number : arr) {
+            sum += number;
+        }
+        System.out.println(sum);
+    }
 
     //Step 3: Find the largest number in the array and print it to the console.
-    public static void findMax(int[] arr){
-        int max=arr[0];
-        for(int number:arr){
-            if(number>max){
-                max=number;
+    public static void findMax(int[] arr) {
+        int max = arr[0];
+        for (int number : arr) {
+            if (number > max) {
+                max = number;
             }
         }
         System.out.println(max);
@@ -60,16 +58,23 @@ public class lesson4Array {
 
     //Step 5: Perform element-wise addition of both arrays and store the result in a third array. Print the result array to the console.
 
-    public static void createArrayOfTwoArrays(int[] firstArr, int[] secondArr){
-        int[] numbers3= new int[firstArr.length];
-        for(int i=0;i<firstArr.length;i++){
-            numbers3[i] =firstArr[i]+secondArr[i];
+    public static void createArrayOfTwoArrays(int[] firstArr, int[] secondArr) {
+        if (firstArr.length <= secondArr.length) {
+            int[] numbers3 = new int[firstArr.length];
+            for (int i = 0; i < firstArr.length; i++) {
+                numbers3[i] = firstArr[i] + secondArr[i];
+            }
+            printArray(numbers3);
+        } else {
+            int[] numbers3 = new int[secondArr.length];
+            for (int i = 0; i < secondArr.length; i++) {
+                numbers3[i] = firstArr[i] + secondArr[i];
+            }
+            printArray(numbers3);
         }
-        for(int result:numbers3){
-            System.out.println(result);
-        }
+
+
     }
-
-
-
 }
+
+
